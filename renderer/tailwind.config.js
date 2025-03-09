c13c63a5498f504a9ab6fe255628d664144e080c
Andrew R <andrewrora@gmail.com>
2025-03-09 13:37:48 -0600

    Fix tailwind config

--- renderer/tailwind.config.js
+++ renderer/tailwind.config.js
@@ -2,6 +2,7 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   content: [
+    './renderer/layout/**/*.{js,ts,jsx,tsx}',
     './renderer/pages/**/*.{js,ts,jsx,tsx}',
     './renderer/components/**/*.{js,ts,jsx,tsx}',
   ],
